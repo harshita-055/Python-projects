a78a51663b62cbd337ada3e167c788fb9c00d954
harshita-055 <73683790+harshita-055@users.noreply.github.com>
2021-07-06 18:34:41 +0530

    Update PhoneBookDirectory.py

--- PhoneBookDirectory.py
+++ PhoneBookDirectory.py
@@ -42,17 +42,91 @@ def Display():
     print("Sorry you dont have any contacts in your phone address book")
     print("Please create it!!!)
   elif(os.stat('Phone_Directory.txt').st_size==0):
-          print("address book is empty")
+    print("address book is empty")
   else:
-          f=open('Phone_Directory.txt','r')
-          text=f.read()
-          print(text)
-          f.close()
+    f=open('Phone_Directory.txt','r')
+    text=f.read()
+    print(text)
+    f.close()
           
 #FUNCTION TO FIND PHONE NUMBER OF A PARTICULAR SUBSCRIBER
 
 def Search():
-  name        
+  name=input("Enter the name:")
+  f=open("Phone_Directory.txt",'r')
+  result=[]
+  for line in f:
+    if name in line:
+      found=True
+      break
+    else:
+      found=False
+  if(found==True):
+    print("The name of person exist in directory:")
+    print(line.replace(',',':'))
+  else:
+    print("The name does not exist in directory")
           
+# FUNCTION TO GET SUBSCRIBER FROM THE USER.THE REQUESTED CHOICE IS RETURNED TO THE MAIN PART IF THE PROGRAM TO PERFORM A PARTICULAR TASK.
   
-    
+def get_choice():
+  print("1)\tAdd new phone number to a list of phone book directory:")no
+  print("2)\tSort name in ascending order")
+  print("3)\tSave all the phone numbers to file")        
+  print("4)\tPrint all phone book directory on the console")
+  print("5)\tSearch phone number from phone directory")  
+  print("6)\tPlease write 6 to exit from the menu ") 
+  ch=input("Please enter the choice")
+  return (ch)
+          
+#MAIN PROGRAM
+          
+if(os.path.isfile("Phone_Directory.txt")==0):
+  print("Sorry you dont have any contacts in your phone book")
+  print("Please create it!!!)
+  directory=[]
+else:
+  print("Already your phone book have some numbers")         
+  print("you can see it!!!)              
+  directory=[]              
+  f=open("Phone_Directory.txt","r")              
+  for line in f:
+         if (line.endwith("\n")):
+                 line=line[:-1]
+                 directory.append(line.strip().split(","))
+f.close()
+#directory=[]
+c=True
+while c:
+        ch= get_choice()
+        if ch=='1':
+          e=Add_Details()
+          directory.append(e)
+        
+        if ch=='2':
+          bub_sort(directory)
+          print("Contents of phone book sorted successfully")
+                
+        if ch=='3':
+          SaveDataToFile(directory)
+          print("Data saved to phone book successfully")
+        
+        if ch=='4':
+          Display()
+        
+        if ch=='5':
+          Search()
+        
+        if ch=='6':
+          print("Thanks a lot for using our application")
+          c=False
+      
+                
+                
+                
+                
+                
+                
+                
+                
+                
